f77daefedaaac4a4453b0b2fbd8002efe621ac22
David Irwin <david@redirwin.com>
2024-10-25 14:39:25 -0700

    fixing missing props on add recipe page

--- app/add-recipe/page.tsx
+++ app/add-recipe/page.tsx
@@ -2,12 +2,18 @@
 
 import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
 import { RecipeForm } from "@/app/components/RecipeForm";
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { Button } from "@/app/components/ui/button";
+import { useRecipes } from "@/lib/recipeContext";
+import { useToast } from "@/app/components/ui/use-toast";
+import { Recipe } from "@/types/Recipe";
 
 export default function AddRecipe() {
   const pathname = usePathname();
   const clerk = useClerk();
+  const router = useRouter();
+  const { addRecipe } = useRecipes();
+  const { toast } = useToast();
 
   const handleSignIn = () => {
     clerk.openSignIn({
@@ -15,10 +21,27 @@ export default function AddRecipe() {
     });
   };
 
+  const handleSave = (recipe: Recipe) => {
+    addRecipe(recipe);
+    toast({
+      title: "Recipe added",
+      description: "Your new recipe has been added successfully.",
+    });
+    router.push("/");
+  };
+
+  const handleCancel = () => {
+    router.push("/");
+  };
+
   return (
     <div className="container mx-auto">
       <SignedIn>
-        <RecipeForm isEditing={false} />
+        <RecipeForm
+          isEditing={false}
+          onSave={handleSave}
+          onCancel={handleCancel}
+        />
       </SignedIn>
       <SignedOut>
         <div className="flex flex-col justify-center h-[30vh]">
